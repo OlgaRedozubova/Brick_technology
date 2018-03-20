7c3b1441ab964228e5c2d54333f926bf5b9b0e32
olga redozubova <olgared82@.gmail.com>
2018-03-20 08:34:41 +0400

    20.03.2018

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
+var isDropDown= document.querySelector('.isDropDown');
 var element = document.getElementsByClassName("dropDown")[0];
+var inputS = document.querySelector('.search__text');
 
-function dropDown_Show() {
- if (element.classList) {
-    element.classList.toggle("visible");
-  } else {
-    // For IE9
-    var classes = element.className.split(" ");
-    var i = classes.indexOf("visible");
+isDropDown.onclick = function (event) {
+  if (event.target !== inputS) {
+    if (element.classList) {
+      element.classList.toggle("visible");
+    } else {
+      // For IE9
+      var classes = element.className.split(" ");
+      var i = classes.indexOf("visible");
 
-    if (i >= 0)
-      classes.splice(i, 1);
-    else
-      classes.push("visible");
-    element.className = classes.join(" ");
+      if (i >= 0)
+        classes.splice(i, 1);
+      else
+        classes.push("visible");
+      element.className = classes.join(" ");
+    }
   }
-}
+};
+
 
